823d6108e188445a7f50eaed2e10f67d1a92ac4e
May Donoban <rppride_2411@hotmail.com>
2025-12-11 22:43:54 -0500

    adding backend turno  models

---
+++ backend/models/Turno.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const turnoSchema = new mongoose.Schema({
+    usuario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    servicio: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Servicio',
+        required: true
+    },
+    fecha: {
+        type: String,
+        required: true
+    },
+    hora: {
+        type: String,
+        required: true
+    },
+    estado: {
+        type: String,
+        enum: ['pendiente', 'pagado', 'cancelado'],
+        default: 'pendiente'
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Turno', turnoSchema);
