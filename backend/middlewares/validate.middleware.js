a56bdf4e91131db230a45b939ee246c30ad7e65e
May Donoban <rppride_2411@hotmail.com>
2025-12-15 19:56:47 -0500

    adding backend middlewares validate

--- backend/middlewares/validate.middleware.js
+++ backend/middlewares/validate.middleware.js
@@ -2,23 +2,25 @@ const { celebrate, Joi, Segments } = require('celebrate');
 
 const schemas = {
     register: {
-        [Segments.BODY]: Joi.object().keys({
+        [Segments.BODY]: Joi.object({
             name: Joi.string().min(3).required(),
             email: Joi.string().email().required(),
-            password: Joi.string().min(6).required(),
-        }),
+            password: Joi.string().min(6).required()
+        })
     },
-
     login: {
-        [Segments.BODY]: Joi.object().keys({
+        [Segments.BODY]: Joi.object({
             email: Joi.string().email().required(),
             password: Joi.string().required()
         })
     }
 };
 
-const validate = (schema) => {
-    return celebrate(schemas[schema]);
+const validate = (schemaName) => {
+    if (!schemas[schemaName]) {
+        throw new Error(`Schema ${schemaName} no existe`);
+    }
+    return celebrate(schemas[schemaName]);
 };
 
 module.exports = { validate };
