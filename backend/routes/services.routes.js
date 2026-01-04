6bc482fd55a4e74029e2c665925f2776bc290070
May Donoban <rppride_2411@hotmail.com>
2026-01-04 15:42:41 -0500

    services.routes.js corrected

--- backend/routes/services.routes.js
+++ backend/routes/services.routes.js
@@ -5,4 +5,25 @@ router.post(
     crearServicio
 );
 
+module.exports = router; const express = require('express');
+const router = express.Router();
+
+const authMiddleware = require('../middlewares/auth.middleware');
+const roleMiddleware = require('../middlewares/role.middleware');
+const {
+    crearServicio,
+    listarServicios
+} = require('../controllers/services.controller');
+
+// Público (usuarios pueden ver servicios)
+router.get('/', listarServicios);
+
+// Solo admin puede crear
+router.post(
+    '/crear-servicio',
+    authMiddleware,
+    roleMiddleware('admin'),
+    crearServicio
+);
+
 module.exports = router;
