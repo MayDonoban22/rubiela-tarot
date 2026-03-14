e033c38b7f2ef9c28111c345cd94f4a4b8afb393
May Donoban <rppride_2411@hotmail.com>
2026-03-14 18:25:58 -0500

    service controller updated

--- backend/controllers/services.controller.js
+++ backend/controllers/services.controller.js
@@ -1,8 +1,11 @@
 const Service = require('../models/Service');
 const AppError = require('../utils/AppError');
 
+
+// ADMIN — Crear servicio
 const crearServicio = async (req, res, next) => {
     try {
+
         const { nombre, descripcion, precio, duracion } = req.body;
 
         if (!nombre || !descripcion || !precio || !duracion) {
@@ -20,21 +23,71 @@ const crearServicio = async (req, res, next) => {
             message: 'Servicio creado correctamente',
             servicio
         });
+
     } catch (error) {
         next(error);
     }
 };
 
+
+// USER — Ver servicios activos
 const listarServicios = async (req, res, next) => {
     try {
-        const servicios = await Service.find();
+
+        const servicios = await Service.find({
+            activo: true
+        });
+
         res.json(servicios);
+
     } catch (error) {
         next(error);
     }
 };
+
+
+// ADMIN — Ver todos
+const listarServiciosAdmin = async (req, res, next) => {
+    try {
+
+        const servicios = await Service.find();
+
+        res.json(servicios);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+// ADMIN — Activar / desactivar
+const toggleServicio = async (req, res, next) => {
+    try {
+
+        const servicio = await Service.findById(req.params.id);
+
+        if (!servicio) {
+            return next(new AppError('Servicio no encontrado', 404));
+        }
+
+        servicio.activo = !servicio.activo;
+
+        await servicio.save();
+
+        res.json({
+            message: "Estado actualizado",
+            servicio
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
 
 module.exports = {
     crearServicio,
-    listarServicios
+    listarServicios,
+    listarServiciosAdmin,
+    toggleServicio
 };
