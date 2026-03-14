c94a6b14cede2ea906e663617e3927d1b67db4f5
May Donoban <rppride_2411@hotmail.com>
2026-03-14 11:47:13 -0500

    sendEmail file added

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (to, subject, html) => {
+
+    try {
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+
+        await transporter.sendMail({
+            from: `"Luz de Rubí" <${process.env.EMAIL_USER}>`,
+            to,
+            subject,
+            html
+        });
+
+        console.log("Email enviado");
+
+    } catch (error) {
+
+        console.log(error);
+
+    }
+
+};
+
+module.exports = sendEmail;
