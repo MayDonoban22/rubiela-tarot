9013f6925fe0e03008979589c8a65452ec273cb2
May Donoban <rppride_2411@hotmail.com>
2026-03-14 18:11:09 -0500

    service updated

--- backend/models/Service.js
+++ backend/models/Service.js
@@ -1,23 +1,33 @@
 const mongoose = require('mongoose');
 
 const servicioSchema = new mongoose.Schema({
+
     nombre: {
         type: String,
         required: true,
         trim: true
     },
+
     descripcion: {
         type: String,
         required: true
     },
+
     precio: {
         type: Number,
         required: true
     },
+
     duracion: {
-        type: Number, // en minutos
+        type: Number,
         required: true
+    },
+
+    activo: {
+        type: Boolean,
+        default: true
     }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('Service', servicioSchema);
