6e1c4c7b9de25d46e259890267c954651c3216cb
May Donoban <rppride_2411@hotmail.com>
2026-03-17 20:04:41 -0500

    cancel and reschedule turn added to turnos.routes

--- backend/routes/turnos.routes.js
+++ backend/routes/turnos.routes.js
@@ -4,19 +4,73 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/auth.middleware');
 
 const {
+
     createTurno,
-    getMyTurnos
+    getMyTurnos,
+    cancelTurno,
+    rescheduleTurno
+
 } = require('../controllers/turnos.controller');
 
+
 // Ver mis turnos
-router.get('/mis-turnos', authMiddleware, getMyTurnos);
+router.get(
+
+    '/mis-turnos',
+
+    authMiddleware,
+
+    getMyTurnos
+
+);
+
 
 // Crear turno
-router.post('/', authMiddleware, createTurno);
+router.post(
+
+    '/',
+
+    authMiddleware,
+
+    createTurno
+
+);
+
+
+// Cancelar turno
+router.patch(
+
+    '/cancel/:id',
+
+    authMiddleware,
+
+    cancelTurno
+
+);
+
+
+// Reagendar turno
+router.patch(
+
+    '/reschedule/:id',
+
+    authMiddleware,
+
+    rescheduleTurno
+
+);
+
 
 // Test
 router.get('/test', (req, res) => {
-    res.json({ message: 'Appointments route funcionando correctamente' });
+
+    res.json({
+
+        message: 'Appointments route funcionando correctamente'
+
+    });
+
 });
+
 
 module.exports = router;
