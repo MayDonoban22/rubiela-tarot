cf6eb27946c3faf39935da22484db7a8bb44b5a1
May Donoban <rppride_2411@hotmail.com>
2026-03-18 19:45:05 -0500

    contact routes updated

--- backend/routes/contact.routes.js
+++ backend/routes/contact.routes.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+
+    sendMessage
+
+} = require('../controllers/contact.controller');
+
+
+// Enviar mensaje contacto
+router.post(
+
+    '/',
+
+    sendMessage
+
+);
+
+
+// Test
 router.get('/test', (req, res) => {
-    res.json({ message: 'Contact route funcionando correctamente' });
+
+    res.json({
+
+        message: 'Contact route funcionando correctamente'
+
+    });
+
 });
 
 module.exports = router;
