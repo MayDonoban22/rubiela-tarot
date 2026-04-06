588e23704d8ce0c96f72459072ea4162efe46c8b
May Donoban <rppride_2411@hotmail.com>
2026-04-06 17:09:05 -0500

    stripeWebhookRoute.js created

---
+++ backend/routes/stripeWebhookRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+const stripeWebhook = require("../controllers/stripeWebhookController");
+
+router.post("/stripe", express.raw({ type: "application/json" }), stripeWebhook);
+
+module.exports = router;
